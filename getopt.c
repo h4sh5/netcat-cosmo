fc9b43b081d19fe0130a5b44b10d7d17882cc1b7
Haoxi Tan <haoxi.tan@gmail.com>
2022-11-13 14:30:06 +1000

    getopt monkey patching

--- getopt.c
+++ getopt.c
@@ -113,7 +113,7 @@
    Also, when `ordering' is RETURN_IN_ORDER,
    each non-option ARGV-element is returned here.  */
 
-char *optarg;
+// char *optarg;
 
 /* Index in ARGV of the next element to be scanned.
    This is used for communication to and from the caller
@@ -128,7 +128,7 @@ char *optarg;
    how much of ARGV has been scanned so far.  */
 
 /* 1003.2 says this must be 1 before any call.  */
-int optind = 1;
+// int optind = 1;
 
 /* Formerly, initialization of getopt depended on optind==0, which
    causes problems with re-calling getopt as programs generally don't
@@ -148,13 +148,13 @@ static char *nextchar;
 /* Callers store zero here to inhibit the error message
    for unrecognized options.  */
 
-int opterr = 1;
+// int opterr = 1;
 
 /* Set to an option character which was unrecognized.
    This must be initialized on some systems to avoid linking in the
    system's own getopt implementation.  */
 
-int optopt = '?';
+// int optopt = '?';
 
 /* Describe how to deal with options that follow non-option ARGV-elements.
 
@@ -1160,17 +1160,17 @@ _getopt_internal (argc, argv, optstring, longopts, longind, long_only)
   }
 }
 
-int
-getopt (argc, argv, optstring)
-     int argc;
-     char *const *argv;
-     const char *optstring;
-{
-  return _getopt_internal (argc, argv, optstring,
-			   (const struct option *) 0,
-			   (int *) 0,
-			   0);
-}
+// int
+// getopt (argc, argv, optstring)
+//      int argc;
+//      char *const *argv;
+//      const char *optstring;
+// {
+//   return _getopt_internal (argc, argv, optstring,
+// 			   (const struct option *) 0,
+// 			   (int *) 0,
+// 			   0);
+// }
 
 #endif	/* Not ELIDE_CODE.  */
 
